71b6f297c76df6d02c2a357e36865f80f8a5b426
ConnaaaR69 <connormattless@gmail.com>
2022-12-21 01:11:09 +0000

    automatic db pushing

--- seed.py
+++ seed.py
@@ -7,6 +7,7 @@
 import csv
 import json
 import random
+import pymysql
 import os
 from classes.Player import Player
 
@@ -14,12 +15,21 @@ pNum = input(
     'Enter number of TEAMS to generate. There are 11 players per team. \n')
 pNum = int(pNum)
 
+cnx = pymysql.connect(user='root', password='',
+                      host='localhost',
+                      database='wcdb')
+
+# Create a cursor
+cursor = cnx.cursor()
 
 # Randomly selects from lists and combines
 # 'playernumber' arg can be changed to alter the number of entries generated
 
 
 def randPlayers(playerNumber=pNum):
+    test = "SELECT max(team_id) FROM players"
+    cursor.execute(test)
+    print(cursor.fetchmany())
 
     print('Generating Players')
 
@@ -30,11 +40,20 @@ def randPlayers(playerNumber=pNum):
     team_id = 1
 
     while i < playerNumber:
-
+        # Generate player
         player = Player(positions[posCounter], team_id)
-        player_list.append(player.getAll())
 
-        print(' Made player: {}'.format(vars(player)))
+        # create sql query to insert player to db
+        query = "INSERT INTO players (first_name, last_name, age, cap, pos, goals_scored, games_played, assists, team_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %d)"
+
+        # run sql query
+        cursor.execute(query, (player.fname, player.lname, player.age, player.cap,
+                       player.pos, player.goals, player.games, player.assists, player.team_id))
+
+        cnx.commit()
+        # player_list.append(player.getAll())
+
+        # print(' Made player: {}'.format(vars(player)))
 
         # iteratives
         posCounter += 1
@@ -99,33 +118,34 @@ if(pNum > 10):
 else:
     randPlayers()
 
+
 # randStaff()
 
 
-# Writes to players.csv
-with open("data/players.csv", "w") as play:
-    # Sets delimitation and line terminations for CSV file
-    player = csv.writer(
-        play,
-        delimiter=",",
-        quotechar='"',
-        quoting=csv.QUOTE_ALL,
-        lineterminator='\r')
+# # Writes to players.csv
+# with open("data/players.csv", "w") as play:
+#     # Sets delimitation and line terminations for CSV file
+#     player = csv.writer(
+#         play,
+#         delimiter=",",
+#         quotechar='"',
+#         quoting=csv.QUOTE_ALL,
+#         lineterminator='\r')
 
-    # Writes player_list array to file
-    player.writerows(player_list)
-# Output how many entries were created, from array len
-print('Created {} entries in players.csv'.format(len(player_list)))
+#     # Writes player_list array to file
+#     player.writerows(player_list)
+# # Output how many entries were created, from array len
+# print('Created {} entries in players.csv'.format(len(player_list)))
 
 
-# Writes to staff.csv
-with open("data/staff.csv", "w") as wtr:
-    # Sets delimitation and line terminations for CSV file
-    staff = csv.writer(
-        wtr,
-        delimiter=",",
-        quotechar='"',
-        quoting=csv.QUOTE_ALL,
-        lineterminator='\r')
-    # Writes player_list array to file
-    staff.writerows(staff_list)
+# # Writes to staff.csv
+# with open("data/staff.csv", "w") as wtr:
+# Sets delimitation and line terminations for CSV file
+# staff = csv.writer(
+#     wtr,
+#     delimiter=",",
+#     quotechar='"',
+#     quoting=csv.QUOTE_ALL,
+#     lineterminator='\r')
+# # Writes player_list array to file
+# staff.writerows(staff_list)
